5221ebe8611a4fc9fb3f76fe2f43bd32c3547a0e
Ardimas <dimasdwisuprayogi@gmail.com>
2024-12-20 16:15:37 +0700

    Moving to utils package

--- src/main/java/com/example/demo/utils/EmailService.java
+++ src/main/java/com/example/demo/utils/EmailService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.utils;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
--- src/main/java/com/example/demo/utils/PasswordResetService.java
+++ src/main/java/com/example/demo/utils/PasswordResetService.java
@@ -1,9 +1,6 @@
-package com.example.demo.service;
+package com.example.demo.utils;
 
 import java.util.UUID;
-
-import javax.management.RuntimeErrorException;
-import javax.transaction.Transactional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
